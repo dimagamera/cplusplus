e664ef05eed67aea463f8c72a73262f93c6ff1cd
Dima Gamera <gamera@step.itstep.rv.ua>
2019-01-29 13:23:18 +0200

    New Class Work

---
+++ Class Work/29.01.2019/29.01.2019/Source.cpp
@@ -0,0 +1,28 @@
+﻿
+//C++
+//Написати функцію, яка приймає три аргументи : години, хвилини, секунди.
+//Функція повинна повертати еквівалент переданого їй тимчасового значення в секундах
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int Plus(int hourse, int minuts , int second) {
+	
+	return hourse * 60 * 60 + minuts * 60 + second;
+
+}
+
+int main() {
+	int hour = 0;
+	int minuts = 0;
+	int second = 0;
+	Plus(hour, minuts, second);
+	cin >> hour >> minuts >> second;
+	cout << "" << Plus(hour, minuts, second) << endl;
+
+	system("pause");
+	return 0;
+}
