a744c581ee7e54358af9dd33bcbc33965147f6c1
gamera dima <gamera1@rivne.itstep.org>
2019-04-16 13:38:54 +0300

    Home Work 31.03.19

---
+++ Class Work/16.04.19/16.04.19/Source.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+
+//Створити структуру з бітовими полями Час(години.хвилини, секунди).
+//Продумати яка мінімальна кількість біт потрібна для збереження певного  елемента структури.Визначити функції :
+//заповнення об’єкта структури з клавіатури
+//вивід  об’єкта структури  на екран
+
+struct Data {
+	unsigned int h : 6;
+	unsigned int m : 6;
+	unsigned int s : 6;
+	unsigned int d : 6;
+	unsigned int mounth : 5;
+	unsigned int y : 11;
+	void set_h(unsigned int new_h) {
+		cin >> new_h;
+		h = new_h;
+	}
+	void set_m(unsigned int new_m) {
+		cin >> new_m;
+		m = new_m;
+	}
+	void set_s(unsigned int new_s) {
+		cin >> new_s;
+		s = new_s;
+	}
+	void set_d(unsigned int new_d) {
+		cin >> new_d;
+		d = new_d;
+	}
+	void set_mounth(unsigned int new_mounth) {
+		cin >> new_mounth;
+		mounth = new_mounth;
+	}
+	void set_y(unsigned int new_y) {
+		cin >> new_y;
+		y = new_y;
+	}
+	void print(){
+		
+		cout << "Year = " << y << " Mounth = " << mounth << " Day = " << d << " Hour = " << h << " Minutes = " << m << " Seconds = " << s << endl;
+	}
+};
+
+int main() {
+
+	Data data;
+	print();
+	
+	//cout << "Enter year: " << endl;
+
+	//cout << "Enter mounth: " << endl;
+
+	//cout << "Enter day: " << endl;
+	//
+	//cout << "Enter Hour: " << endl;
+	//
+	//cout << "Enter minutes" << endl;
+
+	//cout << "Enter seconds" << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Home Work/31.03.2019/31.03.2019/Source.cpp
@@ -0,0 +1,76 @@
+﻿//Написати програму, яка реалізує телефонну книгу
+//з вказаною користувачем кількістю абонентів і всіма необхідними функціями.Використати вкладені структури(для адреси чи ПІБ).
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct booknum
+{
+private:
+	string name;
+	string adres;
+	string telnumb;
+public:
+	void setName(string newname)
+	{
+		name = newname;
+	}
+	void setAdres(string newAdres)
+	{
+		adres = newAdres;
+	}
+	void setTelnum(string newTelnum)
+	{
+		name = newTelnum;
+	}
+	string getName()
+	{
+		return name;
+	}
+	string getAdres()
+	{
+		return adres;
+	}
+	string getTelnum()
+	{
+		return name;
+	}
+};
+int main()
+{
+	unsigned int sizeBook = 0;
+	char varTmp;
+	string tmp;
+	cout << "Enter Size your book = ";
+	cin >> sizeBook;
+	booknum *structArr = new booknum[sizeBook];
+	for (unsigned short i = 0; i < sizeBook; i++)
+	{
+	
+		cout << "Write new people in your numbook\n";
+		cout << "Enter name = ";
+		cin >> tmp;
+		structArr[i].setName(tmp);
+		cout << endl << "Enter Adres = ";
+		cin >> tmp;
+		structArr[i].setAdres(tmp);
+		cout << endl << "Enter Telephone number = ";
+		cin >> tmp;
+		structArr[i].setTelnum(tmp);
+	}
+	for (unsigned short i = 0; i < sizeBook; i++)
+	{
+		if (empty(structArr[i].getName()))
+		{
+			break;
+		}
+		cout << structArr[i].getName() << endl;
+		cout << structArr[i].getAdres() << endl;
+		cout << structArr[i].getTelnum() << endl;
+		cout << "=================================" << endl;
+	}
+	system("pause");
+	delete[] structArr;
+	return 0;
+}
